582a1bd979e8c28af1f83dc22fa51f6f449700df
Pavlo Chorniy <pchorniy@proffiz.com>
2021-10-07 13:17:48 +0300

    configured project

---
+++ app/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="nav">
+    <router-link class="text-green-500" to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
